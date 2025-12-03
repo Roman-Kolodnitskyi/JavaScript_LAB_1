9d6cd44871668a209ffd78ab5c82753242759c8d
Roman-Kolodnitskyi <114834346+Roman-Kolodnitskyi@users.noreply.github.com>
2025-12-03 12:01:30 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,13 @@ function triangle(val1, type1, val2, type2) {
   type1 = type1.toLowerCase();
   type2 = type2.toLowerCase();
 
-  // helpers
+  
   const toRad = d => d * Math.PI / 180;
   const toDeg = r => r * 180 / Math.PI;
 
   let a = null, b = null, c = null, alpha = null, beta = null;
 
-  // LEG + LEG
+  
   if (type1 === "leg" && type2 === "leg") {
     a = val1;
     b = val2;
@@ -17,7 +17,6 @@ function triangle(val1, type1, val2, type2) {
     beta = 90 - alpha;
   }
 
-  // LEG + HYPOTENUSE
   else if ((type1 === "leg" && type2 === "hypotenuse") ||
            (type2 === "leg" && type1 === "hypotenuse")) {
     let leg = type1 === "leg" ? val1 : val2;
@@ -32,7 +31,7 @@ function triangle(val1, type1, val2, type2) {
     beta = 90 - alpha;
   }
 
-  // HYPOTENUSE + ANGLE
+
   else if ((type1 === "hypotenuse" && type2 === "angle") ||
            (type2 === "hypotenuse" && type1 === "angle")) {
     let hyp = type1 === "hypotenuse" ? val1 : val2;
@@ -56,10 +55,11 @@ function triangle(val1, type1, val2, type2) {
   console.log("alpha =", alpha);
   console.log("beta =", beta);
 
-  return "success";
+
 }
 
-// examples:
+
 console.log(triangle(3, "leg", 4, "leg"));
 console.log(triangle(4, "leg", 8, "hypotenuse"));
 console.log(triangle(10, "hypotenuse", 30, "angle"));
+
